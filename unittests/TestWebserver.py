9e26965d8a61029d19c3fb48b9ec9b908ad71faa
OctavianMihaila <mhoctavianiulian@gmail.com>
2024-04-02 15:02:13 +0300

    improved test coverage

--- unittests/TestWebserver.py
+++ unittests/TestWebserver.py
@@ -41,56 +41,101 @@ class TestWebserver(unittest.TestCase):
         for file in os.listdir('../results'):
             os.remove(f'../results/{file}')
 
-    def test_get_results(self):
-        # Test when job_id is valid and job is not in waiting queue
-        response = requests.get('http://127.0.0.1:5000/api/get_results/1')
+    def poll_job_status(self, job_id):
+        # Poll job status until it becomes "done"
+        while True:
+            response = requests.get(f'http://127.0.0.1:5000/api/get_results/{job_id}')
+            data = response.json()
+            if data['status'] == 'done':
+                break
+            time.sleep(1)
+
+    def send_request_and_compare(self, req_data, ref_file, endpoint):
+        # Send POST request
+        response = requests.post("http://127.0.0.1:5000/api/" + endpoint, json=req_data)
         self.assertEqual(response.status_code, 200)
         data = response.json()
-        self.assertEqual(data['status'], 'error')
-
-        # Test when job_id is not valid
-        response = requests.get('http://127.0.0.1:5000/api/get_results/100')
-        self.assertEqual(response.status_code, 200)
-        data = response.json()
-        self.assertEqual(data['status'], 'error')
-
-        # Create a job so that we will have a job_id in the waiting queue
-        req_data = {"question": "Percent of adults aged 18 years and older who have an overweight classification"}
-        response = requests.post("http://127.0.0.1:5000/api/best5", json=req_data)
-        self.assertEqual(response.status_code, 200)
-        job_id = response.json()['job_id']
-
-        # Test when job_id is done or is running
+        
+        # Get job_id
+        job_id = data['job_id']
+        
+        # Poll job status until it becomes "done"
+        self.poll_job_status(job_id)
+        
+        # Get results of the job
         response = requests.get(f'http://127.0.0.1:5000/api/get_results/{job_id}')
         self.assertEqual(response.status_code, 200)
         data = response.json()
-        # It can be both running or done
-        self.assertTrue(data['status'] == 'running' or data['status'] == 'done')
 
-    def test_get_jobs_info(self):
-        response = requests.get('http://127.0.0.1:5000/api/jobs')
-        self.assertEqual(response.status_code, 200)
-        data = response.json()
-        self.assertTrue(isinstance(data, dict))
-        
-    def test_get_num_jobs(self):
-        response = requests.get('http://127.0.0.1:5000/api/num_jobs')
-        self.assertEqual(response.status_code, 200)
-        data = response.json()
-        # Check if the response contains the expected key
-        self.assertTrue('num_jobs' in data)
-        # Check if the value is an integer
-        self.assertTrue(isinstance(data['num_jobs'], int))
-        self.assertTrue(data['num_jobs'] >= 0)
+        # check if the results are as expected
+        with open(ref_file, 'r') as file:
+            ref_data = json.load(file)
+        self.assertEqual(data['data'], ref_data)
 
-    def test_graceful_shutdown(self):
-        response = requests.get('http://127.0.0.1:5000/api/graceful_shutdown')
-        self.assertEqual(response.status_code, 200)
-        data = response.json()
-        # Check if the response contains the expected key
-        self.assertTrue('status' in data)
-        # Check if the status is 'shutting down'
-        self.assertEqual(data['status'], 'shutting down')
+    def test_state_mean_1(self):
+        req_data = {"question": "Percent of adults aged 18 years and older who have obesity", "state": "Ohio"}
+        self.send_request_and_compare(req_data, 'refs/state_mean_1.json', 'state_mean')
+
+    def test_state_mean_2(self):
+        req_data = {"question": "Percent of adults who report consuming vegetables less than one time daily", "state": "Texas"}
+        self.send_request_and_compare(req_data, 'refs/state_mean_2.json', 'state_mean')
+
+    def test_states_mean_1(self):
+        req_data = {"question": "Percent of adults aged 18 years and older who have obesity"}
+        self.send_request_and_compare(req_data, 'refs/states_mean_1.json', 'states_mean')
+
+    def test_states_mean_2(self):
+        req_data = {"question": "Percent of adults who engage in muscle-strengthening activities on 2 or more days a week"}
+        self.send_request_and_compare(req_data, 'refs/states_mean_2.json', 'states_mean')
+
+    def test_best5_1(self):
+        req_data = {"question": "Percent of adults aged 18 years and older who have obesity"}
+        self.send_request_and_compare(req_data, 'refs/best5_1.json', 'best5')
+
+    def test_best5_2(self):
+        req_data = {"question": "Percent of adults who report consuming vegetables less than one time daily"}
+        self.send_request_and_compare(req_data, 'refs/best5_2.json', 'best5')
+
+    def test_worst5_1(self):
+        req_data = {"question": "Percent of adults who report consuming fruit less than one time daily"}
+        self.send_request_and_compare(req_data, 'refs/worst5_1.json', 'worst5')
+
+    def test_worst5_2(self):
+        req_data = {"question": "Percent of adults who engage in no leisure-time physical activity"}
+        self.send_request_and_compare(req_data, 'refs/worst5_2.json', 'worst5')
+
+    def test_global_mean_1(self):
+        req_data = {"question": "Percent of adults aged 18 years and older who have obesity"}
+        self.send_request_and_compare(req_data, 'refs/global_mean_1.json', 'global_mean')
+
+    def test_global_mean_2(self):
+        req_data = {"question": "Percent of adults who report consuming vegetables less than one time daily"}
+        self.send_request_and_compare(req_data, 'refs/global_mean_2.json', 'global_mean')
+
+    def test_diff_from_mean_1(self):
+        req_data = {"question": "Percent of adults aged 18 years and older who have obesity", "state": "Ohio"}
+        self.send_request_and_compare(req_data, 'refs/diff_from_mean_1.json', 'diff_from_mean')
+
+    def test_diff_from_mean_2(self):
+        req_data = {"question": "Percent of adults who report consuming vegetables less than one time daily", "state": "Texas"}
+        self.send_request_and_compare(req_data, 'refs/diff_from_mean_2.json', 'diff_from_mean')
+
+    def test_mean_by_category_1(self):
+        req_data = {"question": "Percent of adults aged 18 years and older who have obesity"}
+        self.send_request_and_compare(req_data, 'refs/mean_by_category_1.json', 'mean_by_category')
+
+    def test_mean_by_category_2(self):
+        req_data = {"question": "Percent of adults who report consuming vegetables less than one time daily"}
+        self.send_request_and_compare(req_data, 'refs/mean_by_category_2.json', 'mean_by_category')
+
+    def test_state_mean_by_category_1(self):
+        req_data = {"question": "Percent of adults aged 18 years and older who have obesity", "state": "Ohio"}
+        self.send_request_and_compare(req_data, 'refs/state_mean_by_category_1.json', 'state_mean_by_category')
+
+    def test_state_mean_by_category_2(self):
+        req_data = {"question": "Percent of adults who report consuming vegetables less than one time daily", "state": "Texas"}
+        self.send_request_and_compare(req_data, 'refs/state_mean_by_category_2.json', 'state_mean_by_category')
+
 
 if __name__ == '__main__':
     unittest.main()
